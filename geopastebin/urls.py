eeac24f5181b398a4b591b019070b8afe4fa734e
Matt Croydon <matt@lawrence.com>
2011-02-08 21:38:55 -0600

    Added form to homepage context and form to example_project template.

--- geopastebin/urls.py
+++ geopastebin/urls.py
@@ -4,7 +4,7 @@ from geopastebin.models import Paste
 from geopastebin.forms import PasteForm
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.list_detail.object_list', kwargs={'queryset' : Paste.objects.public().order_by('-created'), 'paginate_by' : 20}),
+    url(r'^$', 'django.views.generic.list_detail.object_list', kwargs={'queryset' : Paste.objects.public().order_by('-created'), 'paginate_by' : 20, 'extra_context' : {'form' : PasteForm}}),
     url(r'^create/', 'django.views.generic.create_update.create_object', kwargs={'form_class' : PasteForm}, name='geopastebin_create'),
     url(r'^(?P<slug>\w{32})/', 'django.views.generic.list_detail.object_detail', kwargs={'slug_field' : 'uuid', 'queryset' : Paste.objects.all()}, name='geopastebin_detail'),
 )
